0a168ae9ef9719bff149eb0807c84f618b6ba25e
Adryanssyah <adryansyah434@gmail.com>
2024-01-20 21:08:13 +0700

    feat: 404 page

---
+++ src/pages/404Page.tsx
@@ -0,0 +1,11 @@
+import OtherPageLayout from '@/components/layouts/OtherPageLayout';
+
+function PageNotFound() {
+     return (
+          <OtherPageLayout>
+               <h1>404 - Page Not Found</h1>
+          </OtherPageLayout>
+     );
+}
+
+export default PageNotFound;
