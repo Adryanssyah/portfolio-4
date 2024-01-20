80e06c814e8254b6cb951a4097a271962104031d
Adryanssyah <adryansyah434@gmail.com>
2024-01-20 22:49:26 +0700

    fix: get default system color scheme

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -31,6 +31,7 @@ export function ThemeProvider({ children, defaultTheme = 'system', storageKey = 
           if (theme === 'system') {
                const systemTheme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
 
+               root.style.colorScheme = systemTheme;
                root.classList.add(systemTheme);
                return;
           }
