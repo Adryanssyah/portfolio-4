4a2f8d64628bab2d4e759c32b4ae6b2c7af2fe3b
Adryanssyah <adryansyah434@gmail.com>
2024-01-21 16:20:36 +0700

    fix: give the card footer flex wrap

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -19,7 +19,7 @@ CardDescription.displayName = 'CardDescription';
 const CardContent = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(({ className, ...props }, ref) => <div ref={ref} className={cn('pt-0', className)} {...props} />);
 CardContent.displayName = 'CardContent';
 
-const CardFooter = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(({ className, ...props }, ref) => <div ref={ref} className={cn('flex items-center gap-2 pt-0 px-1.5', className)} {...props} />);
+const CardFooter = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(({ className, ...props }, ref) => <div ref={ref} className={cn('flex flex-wrap items-center gap-2 pt-0 px-1.5', className)} {...props} />);
 CardFooter.displayName = 'CardFooter';
 
 export { Card, CardHeader, CardFooter, CardTitle, CardDescription, CardContent };
